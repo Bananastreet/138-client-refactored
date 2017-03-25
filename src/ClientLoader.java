6972ac15eeb18a81f577cd87c4eb6cfbf80ffde8
Chad <nshusa99@gmail.com>
2017-03-25 11:15:16 -0500

    ClientLoader class now properly launches the client

--- src/ClientLoader.java
+++ src/ClientLoader.java
@@ -1,79 +1,73 @@
-
 import java.applet.AppletContext;
 import java.applet.AppletStub;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
 
-public class ClientLoader implements AppletStub {
+/**
+ * The class that will launch the client.
+ * 
+ * @author Freyr
+ */
+public class ClientLoader extends JFrame implements AppletStub {
 
-	private static URL codebase;
-	private static HashMap<String, String> cfg = new HashMap<String, String>();
-	private static HashMap<String, String> params = new HashMap<String, String>();
+	private static final long serialVersionUID = -1657955994522660237L;
+
+	private String host = "http://127.0.0.1";
+	private Client client = new Client();
+	private Map<String, String> params = new HashMap<>();	
 
 	public static void main(String[] args) throws Exception {
-		codebase = new URL(args.length == 0 ? "http://oldschool1.runescape.com" : args[0]);
-		parseParams(new URL(codebase, "jav_config.ws").openStream());
-		Client client = new Client();
-		client.setPreferredSize(new Dimension(Integer.parseInt(cfg.get("applet_minwidth")),
-				Integer.parseInt(cfg.get("applet_minheight"))));
-		client.setStub(new ClientLoader());
-		client.init();
-		String title = new File(".").getCanonicalPath();
-		title = title.substring(title.lastIndexOf('\\') + 1);
-		JFrame frame = new JFrame(title);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setContentPane(client);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		ClientLoader app = new ClientLoader();
+		app.startApplet();
 	}
 
-	private static void parseParams(InputStream stream) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-		String line;
-		while ((line = br.readLine()) != null) {
-			int idx = line.indexOf('=');
-			if (idx != -1) {
-				String key = line.substring(0, idx);
-				String val = line.substring(idx + 1);
-				if (key.equals("param")) {
-					idx = val.indexOf('=');
-					key = val.substring(0, idx);
-					val = val.substring(idx + 1);
-					params.put(key, val);
-				} else {
-					cfg.put(key, val);
-				}
-			}
+	public ClientLoader() {
+		super("OSRS #138");
+		setBackground(Color.BLACK);
+		setPreferredSize(new Dimension(781, 541));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	private void startApplet() {
+		try {
+			params.put("1", "0");
+			params.put("2", "true");
+			params.put("3", "http://www.runescape.com/g=oldscape/slr.ws?order=LPWM");
+			params.put("4", "0");
+			params.put("5", "false");
+			params.put("6", "");
+			params.put("7", ".runescape.com");
+			params.put("8", "ElZAIrq5NpKN6D3mDdihco3oPeYN2KFy2DCquj7JMmECPmLrDP3Bnw");
+			params.put("9", "0");
+			params.put("10", "false");
+			params.put("11", "301");			
+			params.put("12", "4665");
+			params.put("13", "0");
+			params.put("14", "5");
+			params.put("15", "0");
+
+			client.setStub(this);
+
+			add(client);
+			pack();
+			setVisible(true);
+
+			client.init();
+			client.start();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
 	@Override
-	public boolean isActive() {
-		return false;
-	}
-
-	@Override
-	public URL getDocumentBase() {
-		return codebase;
-	}
-
-	@Override
-	public URL getCodeBase() {
-		return codebase;
-	}
-
-	@Override
-	public String getParameter(String name) {
-		return params.get(name);
+	public void appletResize(int width, int height) {
+		client.resize(width, height);
 	}
 
 	@Override
@@ -82,7 +76,28 @@ public class ClientLoader implements AppletStub {
 	}
 
 	@Override
-	public void appletResize(int width, int height) {
+	public URL getCodeBase() {
+		try {
+			return new URL(host);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
+	@Override
+	public URL getDocumentBase() {
+		try {
+			return new URL(host);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public String getParameter(String key) {
+		return params.get(key);
+	}
+	
 }
